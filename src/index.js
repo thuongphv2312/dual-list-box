f86dcc8fce7413dfa6f9a14db7c118e56eb5d4c8
Pika Thuong <thuongphv2312@gmail.com>
2022-07-17 22:50:33 +0700

    format code

--- src/index.js
+++ src/index.js
@@ -6,15 +6,11 @@ const app = express();
 let port = 8080;
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-  extended: true
-}))
+app.use(bodyParser.urlencoded({extended: true}))
 
 viewEngine(app);
 initWebRoutes(app);
 
-
-
 app.listen(port, () => {
-  console.log('Server is running on port ', port);
+    console.log('Server is running on port ', port);
 })
